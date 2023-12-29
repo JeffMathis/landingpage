64d621e801512b383dfbc72c51d8e8f57ef5c377
jeff <jmathis7113@gmail.com>
2023-12-28 19:59:39 -0500

    s

--- fades.js
+++ fades.js
@@ -2,40 +2,48 @@ const hirebtn = document.querySelector(".floating-btn");
 const hiresection = document.querySelector(".cta");
 const sectionFade = document.querySelectorAll(".section-fade");
 const sectionObserved = document.querySelectorAll(".section-observed");
-const sectionFadeRight = document.querySelectorAll(".section-fade-right")
+const sectionFadeRight = document.querySelectorAll(".section-fade-right");
 
 const options = {
-    threshold: 0.40
+  threshold: 0.4,
 };
 
 const sectionoptions = {
-    threshold: .40
+  threshold: 0.4,
 };
 
-const hirebtnobserver = new IntersectionObserver(function(entries, hirebtnobserver) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            hirebtn.classList.remove("disappear");
-        } else {
-            hirebtn.classList.add("disappear")
-        }
-    });
-}, options);
+const hirebtnobserver = new IntersectionObserver(function (
+  entries,
+  hirebtnobserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      hirebtn.classList.toggle("disappear");
+    } else {
+      hirebtn.classList.add("disappear");
+    }
+  });
+},
+options);
 
-const sectionObserver = new IntersectionObserver(function (entries, sectionObserver) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            entry.target.classList.add("fadeout");
-        } else {
-            entry.target.classList.remove("fadeout");
-            entry.target.classList.add("fadein");
-            sectionObserver.unobserve(entry.target);
-        }
-    });
-}, sectionoptions);
+const sectionObserver = new IntersectionObserver(function (
+  entries,
+  sectionObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      entry.target.classList.add("fadeout");
+    } else {
+      entry.target.classList.remove("fadeout");
+      entry.target.classList.add("fadein");
+      sectionObserver.unobserve(entry.target);
+    }
+  });
+},
+sectionoptions);
 
 hirebtnobserver.observe(hiresection);
 
-sectionFade.forEach(sectionFade => {
-    sectionObserver.observe(sectionFade);
+sectionFade.forEach((sectionFade) => {
+  sectionObserver.observe(sectionFade);
 });
