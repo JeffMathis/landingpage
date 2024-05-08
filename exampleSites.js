49f6d5cecbe517a114a95d60de3558106744ed64
florp <jmathis7113@gmail.com>
2024-05-08 17:39:19 -0400

    redesign, completely forgot what i did...

--- exampleSites.js
+++ exampleSites.js
@@ -1,6 +1,7 @@
-// for buttons
 
-const btn = document.querySelectorAll(".example-btn")
+const btn = document.querySelectorAll(".example-btn");
+const img = document.getElementById('example-img');
+
 
 btn.forEach((btn) => {
     btn.addEventListener('click', () => {
@@ -10,39 +11,25 @@ btn.forEach((btn) => {
     });
 });
 
-//for image
 
-const img = document.getElementById('example-img');
+const btn1 = document.getElementById('btn1').addEventListener('click', () => {
+    const hero = document.querySelector('.hero-section');
+    document.querySelector('.active')?.classList.remove('active')
 
-// const btn1 = document.getElementById('btn1').addEventListener('click', ()  => {
-//     if (window.screen.width < 730) {
-//         img.src = './assets/hcmobilehero.png'
-//     } else {
-//         img.src = './assets/hcdesktop.png'
-//     }
-// });
+    hero.classList.add('active')
+})
 
-// const btn2 = document.getElementById('btn2').addEventListener('click', () => {
-//     if (window.screen.width < 730) {
-//         img.src = './assets/himobile.png'
-//     } else {
-//         img.src = './assets/hidesktop.png'
-//     }
-// });
+const btn2 = document.getElementById('btn2').addEventListener('click', () => {
+    const mobile = document.querySelector('.mobile-designs');
+    document.querySelector('.active')?.classList.remove('active')
 
-// const btn3 = document.getElementById('btn3').addEventListener('click', () => {
-//     if (window.screen.width < 730) {
-//         img.src = './assets/cvmobile.png'
-//     } else {
-//         img.src = './assets/cvdesktop.png'
-//     }
-// });
+    mobile.classList.add('active')
+})
 
-const btn1 = document.getElementById('btn1').addEventListener('click', () => img.src = './assets/hcdesktop.png')
+const btn3 = document.getElementById('btn3').addEventListener('click', () => {
+    const forms = document.querySelector('.form-designs');
+    document.querySelector('.active')?.classList.remove('active')
 
-const btn2 = document.getElementById('btn2').addEventListener('click', () => img.src = './assets/hidesktop.png')
-
-const btn3 = document.getElementById('btn3').addEventListener('click', () => img.src = './assets/cvdesktop.png')
-
-const btn4 = document.getElementById('btn4').addEventListener('click', () => img.src = './assets/server.png')
+    forms.classList.add('active')
+})
 
